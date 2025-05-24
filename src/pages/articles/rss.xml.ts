0004797531d2a6a7e38f09e14e4ec5ce85be9a34
Erdal Toprak <contact@erdaltoprak.com>
2025-05-24 00:23:23 +0000

    Fixing RSS feeds

--- src/pages/articles/rss.xml.ts
+++ src/pages/articles/rss.xml.ts
@@ -5,19 +5,37 @@ import MarkdownIt from 'markdown-it';
 const parser = new MarkdownIt();
 
 export async function GET(context: { site: string }) {
+  const site_url = context.site;
   // @ts-ignore
   const articles = await getCollection('articles');
   return rss({
     title: 'Erdal Toprak – Articles',
     description: 'Long-form articles',
-    site: context.site,
+    site: site_url,
     stylesheet: '/rss/styles.xsl',
-    items: articles.map((post) => ({
-      link: `/articles/${post.slug}/`,
-      content: sanitizeHtml(parser.render(post.body), {
-        allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
-      }),
-      ...post.data,
-    })),
+    items: articles.map((post) => {
+      const post_content = sanitizeHtml(
+        parser.render(post.body),
+        {
+          allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
+          transformTags: {
+            'img': (tagName, attribs) => {
+              if (attribs.src && attribs.src.startsWith('./')) {
+                attribs.src = `${site_url}/articles/${post.slug}/${attribs.src.substring(2)}`;
+              }
+              return { tagName, attribs };
+            }
+          }
+        }
+      );
+      return {
+        ...post.data,
+        author: 'rss@erdaltoprak.com',
+        link: `/articles/${post.slug}/`,
+        content: post_content,
+        pubDate: new Date(post.data.pubDate),
+      };
+    }),
+    customData: `<atom:link href="${site_url}/articles/rss.xml" rel="self" type="application/rss+xml" xmlns:atom="http://www.w3.org/2005/Atom"/>`,
   });
 } 
--- src/pages/notes/rss.xml.ts
+++ src/pages/notes/rss.xml.ts
@@ -5,19 +5,37 @@ import MarkdownIt from 'markdown-it';
 const parser = new MarkdownIt();
 
 export async function GET(context: { site: string }) {
+  const site_url = context.site;
   // @ts-ignore
   const notes = await getCollection('notes');
   return rss({
     title: 'Erdal Toprak – Notes',
     description: 'Short-form notes',
-    site: context.site,
+    site: site_url,
     stylesheet: '/rss/styles.xsl',
-    items: notes.map((post) => ({
-      link: `/notes/${post.slug}/`,
-      content: sanitizeHtml(parser.render(post.body), {
-        allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
-      }),
-      ...post.data,
-    })),
+    items: notes.map((post) => {
+      const post_content = sanitizeHtml(
+        parser.render(post.body),
+        {
+          allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
+          transformTags: {
+            'img': (tagName, attribs) => {
+              if (attribs.src && attribs.src.startsWith('./')) {
+                attribs.src = `${site_url}/notes/${post.slug}/${attribs.src.substring(2)}`;
+              }
+              return { tagName, attribs };
+            }
+          }
+        }
+      );
+      return {
+        ...post.data,
+        author: 'rss@erdaltoprak.com',
+        link: `/notes/${post.slug}/`,
+        content: post_content,
+        pubDate: new Date(post.data.pubDate),
+      };
+    }),
+    customData: `<atom:link href="${site_url}/notes/rss.xml" rel="self" type="application/rss+xml" xmlns:atom="http://www.w3.org/2005/Atom"/>`,
   });
 } 
